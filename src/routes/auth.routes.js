17327aa27a6b88a46d3514b47b97fe2a55052b87
Ruturaj-007 <pruturaj3003@gmail.com>
2026-07-11 22:21:28 +0530

    feat(auth): confirming access token worked perfectly in multiple cases

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,8 @@ const validate = require('../middlewares/validate.middleware');
 const { registerSchema } = require('../validations/auth.validation');
 
 const router = express.Router();
-
+const protect = require('../middlewares/auth.middleware');
+router.get('/me', protect, (req, res) => res.json({ success: true, user: req.user }));
 router.post('/register', validate(registerSchema), register);
 
 module.exports = router;
